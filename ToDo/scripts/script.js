8fd627c90edd715eafb4b7c614efd43269e05f1c
Tyler Hawkins <thawkins@youniqueproducts.com>
2017-09-18 22:25:33 -0600

    added editing feature

--- ToDo/scripts/script.js
+++ ToDo/scripts/script.js
@@ -8,16 +8,16 @@ Vue.component('todo-item', {
   template: '\
     <li>\
       <input type="checkbox" />\
-      <div>{{ title }}</div>\
-      <input type="text" v-model="title" />\
+      <span class="todoText" v-show="!editing">{{ title }}</span>\
+      <input type="text" v-model="title" v-show="editing" />\
       <div class="buttons">\
-        <button class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-edit"></i></button>\
+        <button v-on:click="$emit(\'edit\')" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-edit"></i></button>\
         <button class="handle btn btn-sm btn-success"><i class="glyphicon glyphicon-move"></i></button>\
         <button v-on:click="$emit(\'remove\')" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></button>\
       </div>\
     </li>\
   ',
-  props: ['title']
+  props: ['title', 'editing']
 });
 
 var toDoExample = new Vue({
@@ -28,14 +28,17 @@ var toDoExample = new Vue({
       {
         id: 1,
         title: 'Do the dishes',
+        editing: false
       },
       {
         id: 2,
         title: 'Take out the trash',
+        editing: false
       },
       {
         id: 3,
-        title: 'Mow the lawn'
+        title: 'Mow the lawn',
+        editing: false
       }
     ],
     nextTodoId: 4
